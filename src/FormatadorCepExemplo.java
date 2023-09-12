767fe1522836d83bdd13c234b265d2b662131cad
Redesu <willyanmaster@gmail.com>
2023-09-12 14:11:55 -0300

    Try catch exercises

---
+++ src/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadorCepExemplo {
+    
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Cep deve conter 8 digitos");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+        throw new CepInvalidoException();
+        return "23.765-064";
+
+    
+    }
+
+}
